baa45f3a9a1822385725c38b47a9c65d09761db3
Cassia Medeiros <69870807+Cassia-Medeiros@users.noreply.github.com>
2021-01-24 21:50:04 -0300

    Hello world

---
+++ kt_000_hello_world/src/Principal.kt
@@ -0,0 +1,13 @@
+
+
+
+fun main() {  // visibilidade para todo o projeto
+    println("Hello World")
+
+    val f01 = Funcionario("Hugo", 52)
+
+    f01.nome = "N.Hugo"
+
+    println(f01.nome)
+}
+
